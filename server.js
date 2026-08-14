467d3c80885e46ff1d2483d3f1f038ebea9e8853
samuelbadcock15-web <samuelbadcock15@gmail.com>
2026-08-14 23:12:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running smoothly on http://localhost:${PORT}`);
+    console.log(`Server is running smoothly on port ${PORT}`);
 });
