2a3c681331e8864d6998cf980e2cd139fd5e64f7
samuelbadcock15-web <samuelbadcock15@gmail.com>
2026-08-17 11:28:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 let waitingPlayer = null;
